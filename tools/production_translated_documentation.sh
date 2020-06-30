778fde825b101a2b23427987a4bd8499f885bf8b
SooluThomas <soolu.elto@gmail.com>
2020-06-30 09:50:04 -0400

    Update the script to generate the build files in build/ directory

--- tools/production_translated_documentation.sh
+++ tools/production_translated_documentation.sh
@@ -18,7 +18,7 @@
 SOURCE_REPOSITORY="https://github.com/Qiskit/qiskit.git"
 TARGET_REPOSITORY="git@github.com:SooluThomas/qiskit-translations.git"
 TARGET_DOC_DIR="documentation/"
-SOURCE_DOC_DIR="docs/_build/html/"
+SOURCE_DOC_DIR="docs/_build/"
 SOURCE_DIR=`pwd`
 TARGET_BRANCH="gh-pages"
 
@@ -38,31 +38,7 @@ pushd $SOURCE_DIR/docs
 # Make translated document
 sphinx-build -b html -j auto -D content_prefix=documentation -D language=$TRANSLATION_LANG . _build/html/locale/$TRANSLATION_LANG
 
-git clone https://SooluThomas:$REPO_TOKEN@github.com/SooluThomas/qiskit-translations.git --branch=gh-pages gh-pages
-
-echo "Make directory locale/"
-mkdir -p $TARGET_DOC_DIR
-echo "made directory"
-
-echo "List files"
-ls
-
-echo "copy html files to locale/"
-cp -r $SOURCE_DIR/$SOURCE_DOC_DIR/* gh-pages/$TARGET_DOC_DIR/
-
-cd gh-pages
-ls
-
-git remote set-url origin https://SooluThomas:$REPO-TOKEN@github.com/SooluThomas/qiskit-translations.git
-
-git config user.name "SooluThomas"
-git config user.email "soolu.elto@gmail.com"
-
-echo "git add"
-git add $TARGET_DOC_DIR
-
-# Commit and push the changes.
-git commit -m "Automated documentation update from meta-qiskit"
-git push --quiet origin $TARGET_BRANCH
+echo "move html files from _build/ to build/"
+mv $SOURCE_DIR/$SOURCE_DOC_DIR/* build/
 
 popd
